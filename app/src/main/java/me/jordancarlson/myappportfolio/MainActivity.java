3213958d2c4b863724bf2baf7ce80820b707694f
Jordan Carlson <jordancarlson08@gmail.com>
2015-06-03 20:44:04 -0600

    feat: Buttons and Toast Messages

--- app/src/main/java/me/jordancarlson/myappportfolio/MainActivity.java
+++ app/src/main/java/me/jordancarlson/myappportfolio/MainActivity.java
@@ -1,38 +1,60 @@
 package me.jordancarlson.myappportfolio;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
-public class MainActivity extends ActionBarActivity {
+/**
+ * Main Activity for simple portfolio launcher app.
+ */
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.inject(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        // Init Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call some material design APIs here
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.blue_700));
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.spotifyButton) void onSpotifyClick() {
+        Toast.makeText(this, "Launch Spotify Streamer app!", Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.scoresButton) void onScoresClick() {
+        Toast.makeText(this, "Launch Scores app!", Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.libraryButton) void onLibraryClick() {
+        Toast.makeText(this, "Launch Library app!", Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.buildButton) void onBuildClick() {
+        Toast.makeText(this, "Launch Build It Bigger app!", Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.xyzButton) void onXyzClick() {
+        Toast.makeText(this, "Launch XYZ Reader app!", Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.capstoneButton) void onCapstoneClick() {
+        Toast.makeText(this, "Launch Capstone app!", Toast.LENGTH_SHORT).show();
     }
 }
