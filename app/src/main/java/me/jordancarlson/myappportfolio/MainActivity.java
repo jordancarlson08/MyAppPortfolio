03b066120588a67c6f2b898b83ff0ce37c0c4f67
Jordan Carlson <jordancarlson08@gmail.com>
2015-06-07 23:11:50 -0600

    feat: Added intent to open Spotify Streamer app

--- app/src/main/java/me/jordancarlson/myappportfolio/MainActivity.java
+++ app/src/main/java/me/jordancarlson/myappportfolio/MainActivity.java
@@ -1,5 +1,7 @@
 package me.jordancarlson.myappportfolio;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -40,7 +42,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.spotifyButton) void onSpotifyClick() {
-        Toast.makeText(this, "Launch Spotify Streamer app!", Toast.LENGTH_SHORT).show();
+        PackageManager packageManager = getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage("me.jordancarlson.spotifystreamer");
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(intent);
+//        Toast.makeText(this, "Launch Spotify Streamer app!", Toast.LENGTH_SHORT).show();
     }
     @OnClick(R.id.scoresButton) void onScoresClick() {
         Toast.makeText(this, "Launch Scores app!", Toast.LENGTH_SHORT).show();
